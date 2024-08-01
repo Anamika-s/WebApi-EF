abc847fe1b43609db3a9eb3dcbe48eff46e56949
Anamika Sawhney <anamika.sawhney22@gmail.com>
2024-08-01 19:13:23 +0530

    Add project files.

---
+++ WebApi-EF/Context/StudentDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApi_Demo.Models;
+
+namespace WebApi_EF.Context
+{
+    public class StudentDbContext : DbContext
+    {
+        public StudentDbContext(DbContextOptions<StudentDbContext> options) : base(
+            options) { }
+
+        public DbSet<Student> Students{ get; set; }
+    }
+}
